767d7420705490f88b691b898469f864e7aa7c60
mkbc3 <leonia.klebanov@gmail.com>
2020-01-08 14:24:05 +0200

    1. encapsulation concept

--- src/com/bottomline/Account.java
+++ src/com/bottomline/Account.java
@@ -3,13 +3,24 @@ package com.bottomline;
 public class Account {
     private float balance;
 
+    public void deposit(float amount){
+        if(amount > 0) {
+            balance += amount;
+        }
+    }
+    public void withdraw(float amount){
+        if(balance > amount) {
+            balance -= amount;
+        }
+    }
+    // provide view for encapsulation data using private attributes
     public void setBalance(float balance){
         // ToDo: add exception to warn about negative balance
         if (balance > 0) {
             this.balance = balance;
         }
     }
-
+    // provide view for encapsulation data using private attributes
     public float getBalance(){
         return balance;
     }
