9e09186c4b268805743bf14aa0240a9a209342c2
mkbc3 <leonia.klebanov@gmail.com>
2020-01-08 14:47:59 +0200

    1. illustrate interface concept

--- src/com/bottomline/TaxCalculator.java
+++ src/com/bottomline/TaxCalculator.java
@@ -1,5 +1,7 @@
 package com.bottomline;
 
+// illustrate interface OOP concept
+// interface: provide implementation staff for derived classes
 public interface TaxCalculator {
     float calculateTax();
 }
