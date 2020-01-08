53bf0ad3c24156c723cf68a8b35ffdec54502b91
mkbc3 <leonia.klebanov@gmail.com>
2020-01-08 15:05:04 +0200

    1. abstraction: added. 2. by changing un required methods to private.

--- src/com/bottomline/MailService.java
+++ src/com/bottomline/MailService.java
@@ -3,5 +3,23 @@ package com.bottomline;
 // illustrate OOP abstraction concept
 // abstraction: reduce complexity meaning hide the un required details
 public class MailService {
-
+    // illustrate abstraction: only method available to user, other methods hided means private
+    public void sendEmail(){
+        connect();
+        authenticate();
+        // send email
+        disconnect();
+    }
+    // illustrate abstraction: private: hiding un required details from class user
+    private void connect(){
+        System.out.println("Connect...");
+    }
+    // illustrate abstraction: private: hiding un required details from class user
+    private void disconnect(){
+        System.out.println("Disconnect...");
+    }
+    // illustrate abstraction: private: hiding un required details from class user
+    private void authenticate(){
+        System.out.println("Authenticate");
+    }
 }
