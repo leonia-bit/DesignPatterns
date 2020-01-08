7af2761e73930bfef09adac7522a5c365da5e11f
mkbc3 <leonia.klebanov@gmail.com>
2020-01-08 15:08:36 +0200

    1. abstraction: added.

--- src/com/bottomline/Main.java
+++ src/com/bottomline/Main.java
@@ -3,22 +3,25 @@ package com.bottomline;
 public class Main {
 
     public static void main(String[] args) {
-        // first java class with one attribute and one method
+        // java class
 	    User user = new User("Moshe", 30);
 	    System.out.println(user.getUserName());
 	    user.sayHello();
 
-	    // first java interface that calculate tax for diff years
+	    // interface
 	    TaxCalculator calculator = getCalculator();
         System.out.println(calculator.calculateTax());
 
-        // core object oriented concept: encapsulation
+        // encapsulation
         var account = new Account();
         account.deposit(10);
         account.withdraw(5);
         System.out.println(account.getBalance());
 
-        // core object oriented concept: abstraction : reduce complexity
+        // abstraction
+        var mailService  = new MailService();
+        mailService.sendEmail();
+
 
     }
 
