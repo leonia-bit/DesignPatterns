258a5a66412fe34e179e25347977c6abdf9322cc
mkbc3 <leonia.klebanov@gmail.com>
2020-01-09 09:30:23 +0200

    1.call to different OOP concept added

--- src/com/bottomline/Main.java
+++ src/com/bottomline/Main.java
@@ -4,26 +4,47 @@ public class Main {
 
     public static void main(String[] args) {
         // java class
+        // it's mechanism to manipulates class attributes using class methods
 	    User user = new User("Moshe", 30);
 	    System.out.println(user.getUserName());
 	    user.sayHello();
 
 	    // interface
+        // it's mechanism that ask th derived classes to implement base class behaviour
 	    TaxCalculator calculator = getCalculator();
         System.out.println(calculator.calculateTax());
 
         // encapsulation
+        // it's mechanism to operate on class attributes
         var account = new Account();
         account.deposit(10);
         account.withdraw(5);
         System.out.println(account.getBalance());
 
         // abstraction
+        // it's mechanism to hide un required details from class user
         var mailService  = new MailService();
         mailService.sendEmail();
 
+        // inheritance
+        // it's mechanism to reuse a code
+        var textBox = new TextBox();
+        textBox.enable();
+
+        // polymorphism
+        // single object type can have many forms
+        drawUIControl(new TextBox());
+        drawUIControl(new CheckBox());
+
+        // UML: Unified Modeling Lang
+
 
     }
+
+    public static void drawUIControl(UIControl control){
+        control.draw();
+    }
+
 
     public static TaxCalculator getCalculator(){
         return new TaxCalculator2019();
